023b88ea13da0d5a3fbdeb162a6f22ca5d10723f
Gabriela Nery <gneri94@gmail.com>
2021-10-14 15:29:16 -0300

    Adicionar arquivos de projeto.

---
+++ verivox/Models/Tariff.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace verivox.Models
+{
+    public class Tariff
+    {
+        public string Name { get; set; }
+        public double AnnualCosts { get; set; }
+
+        public void CalculateAnnualCosts(double input)
+        {
+            switch (Name)
+            {
+                case "Basic electricity tariff":
+                    AnnualCosts = input * 0.22  >60 ? input * 0.22 : 60;
+                    break;
+
+                case "Packaged tariff":
+                    AnnualCosts = input < 4000 ? 800 : ((800)+((input - 4000)*.30));
+                    break;
+
+                default:
+                    Console.WriteLine($"Measured value i; too low.");
+                    break;
+            }
+
+        }
+
+    }
+}
